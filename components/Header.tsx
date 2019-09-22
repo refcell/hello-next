541272e9f1723e29cdea83d983ec628ec57983be
Andreas Bigger <bigger@usc.edu>
2019-09-21 17:00:28 -0700

    playing around with prefetching

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+
+const linkStyle = {
+  marginRight: 15
+}
+
+export default function Header() {
+  return (
+    <div>
+      <Link prefetch href="/">
+        <a style={linkStyle}>Home</a>
+      </Link>
+      <Link prefetch href="/about">
+        <a style={linkStyle}>About</a>
+      </Link>
+      <Link prefetch href="/dashboard">
+        <a style={linkStyle}>Dashboard</a>
+      </Link>
+    </div>
+  )
+}
---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/MyLayout.jsx'
+
+export const config = { amp: 'hybrid' };
+
+export default function About() {
+  return (
+    <Layout>
+      <p>This is the about page</p>
+    </Layout>
+  )
+}
